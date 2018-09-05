93ab397949f5293b70043a99e83667c53996f966
Zac Willmington <zac.willmington@gmail.com>
2018-09-05 22:23:47 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -30,7 +30,7 @@ function calculatesFarePrice(start, destination){
     let price;
     let distance = distanceTravelledInFeet(start, destination);
      if (distance <= 2000 && distance >= 400){
-         price = distance / .02 * 100 - 400;
+         price = (distance - 400) * .02;
      } else if (distance > 2000){
          price = distance * 25;
      } else if (distance < 400){
