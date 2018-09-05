d70e67fafadf84139c48c65544b1b15fdfff278c
Zac Willmington <zac.willmington@gmail.com>
2018-09-05 21:49:19 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -1,6 +1,9 @@
 // Code your solution in this file!
 // HQ is 42nd
 function distanceFromHqInBlocks(distance){
-    let blocks = distance - 42;
+    let blocks;
+    if (distance < 42 ){
+        let blocks = distance - 42;
+    }
     return blocks; 
 }
