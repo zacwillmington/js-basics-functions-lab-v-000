01f3f3bba71e45c724b4312195f1765b7e2f5f0e
Zac Willmington <zac.willmington@gmail.com>
2018-09-05 21:51:32 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -2,7 +2,7 @@
 // HQ is 42nd
 function distanceFromHqInBlocks(distance){
     let blocks;
-    if (distance <= 42 ){
+    if (distance >= 42 ){
         blocks = distance - 42;
     } else {
         // blocks = distance + 42;
